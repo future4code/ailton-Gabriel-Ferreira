c9aef2cea595c16fcb1309ba1aff9c4778b3c6d1
Gabriel Theodoro Ferreira <gabthefer1@gmail.com>
2022-05-10 13:51:13 -0300

    Exercicio de Operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,42 @@
+// Exercicios comentados
+// 1 - a. false, b. false, c. true, d. boolean
+
+// 2 - O problema é que ele está somando resultados do tipo string e no console será mostrado os dois numeros concatenados
+
+// 3 - Para conseguir somar os valores deverá transformar os resultados do tipo string em numero usando Number(primeiroNumero)+Number(segundoNumero)
+
+//Exercicios de codigo
+// 1:
+
+let idadeUsuario = +prompt('Qual sua idade')
+let idadeAmigo = +prompt('Qual a idade do seu melhor amigo')
+let comparacao = idadeUsuario > idadeAmigo
+console.log('Sua idade é maior do que a do seu melhor amigo?,', comparacao)
+console.log('A diferença de idade entre você e seu amigo é', idadeUsuario - idadeAmigo)
+
+// 2:
+
+let numeroPar = +prompt('Insira um numero par')
+let resto = numeroPar % 2
+console.log (resto)
+
+// Caso o usuario insira um numero impar o resultado será 1
+
+//3:
+
+let idade = prompt('Qual sua idade')
+let idadeMeses = idade*12
+let idadeDias = idade*365
+let idadeHoras = idade*365*24
+console.log(`Sua idade em meses é ${idadeMeses}, Sua idade em dias é ${idadeDias}, Sua idade em horas é ${idadeHoras}`)
+
+//4:
+
+let numero1 = Number(prompt('Insira um numero'))
+let numero2 = Number(prompt('Insitra outro numero'))
+console.log(`${numero1} é maior que ${numero2}? ${numero1>numero2}`)
+console.log(`${numero1} é igual a ${numero2}? ${numero1===numero2}`)
+let resto = numero1 % numero2
+let restoInverso = numero2 % numero1
+console.log(`${numero1} é divisivel por ${numero2}? ${resto === 0}`)
+console.log(`${numero2} é divisivel por ${numero1}? ${restoInverso === 0}`)
