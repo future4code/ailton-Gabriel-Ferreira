ff79e2e92a5b76cf3d21b1ef938125560a4173e0
Gabriel Theodoro Ferreira <gabthefer1@gmail.com>
2022-08-17 17:25:28 -0300

    Exercicios sem desafio

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,11 @@
+type product = {
+  id: string;
+  name: string;
+  price: number;
+};
+
+export const products: product[] = [
+    { id: "1", name: "Carrinho", price: 1 },
+    { id: "2", name: "Camiseta", price: 3 },
+    { id: "3", name: "Caneca", price: 1 }
+];
